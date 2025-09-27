66999e563576532b3a54c0572314b7e2e8bfc262
Fernando Borretti <fernando@borretti.me>
2025-09-28 09:50:39 +1000

    Timestamp type, insert_session

--- src/new_db.rs
+++ src/new_db.rs
@@ -16,6 +16,8 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::MutexGuard;
 
+use chrono::DateTime;
+use chrono::Utc;
 use rusqlite::Connection;
 use rusqlite::ToSql;
 use rusqlite::Transaction;
@@ -129,6 +131,43 @@ fn insert_card(tx: &Transaction, card: &CardRow) -> Fallible<()> {
             card.cloze_end,
         ),
     )?;
+    Ok(())
+}
+
+pub struct Timestamp(DateTime<Utc>);
+
+impl Timestamp {
+    pub fn now() -> Self {
+        Self(Utc::now())
+    }
+}
+
+impl From<DateTime<Utc>> for Timestamp {
+    fn from(dt: DateTime<Utc>) -> Self {
+        Timestamp(dt)
+    }
+}
+
+impl ToSql for Timestamp {
+    fn to_sql(&self) -> rusqlite::Result<ToSqlOutput<'_>> {
+        let str = self.0.to_rfc3339();
+        Ok(ToSqlOutput::from(str))
+    }
+}
+
+impl FromSql for Timestamp {
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+        let string: String = FromSql::column_result(value)?;
+        let ts =
+            DateTime::parse_from_rfc3339(&string).map_err(|e| FromSqlError::Other(Box::new(e)))?;
+        let ts = ts.with_timezone(&Utc);
+        Ok(Timestamp::from(ts))
+    }
+}
+
+fn insert_session(tx: &Transaction, started_at: Timestamp, ended_at: Timestamp) -> Fallible<()> {
+    let sql = "insert into sessions (started_at, ended_at) values (?, ?);";
+    tx.execute(sql, (started_at, ended_at))?;
     Ok(())
 }
 
