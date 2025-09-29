d6a664811bcc8a85ee99b030910f5519533f441d
Fernando Borretti <fernando@borretti.me>
2025-09-29 17:43:21 +1000

    wip vue frontend

---
+++ src/frontend/README.md
@@ -0,0 +1,8 @@
+This is the code for the hashcards frontend.
+
+```
+pnpm install
+pnpm dev
+pnpm build
+pnpm lint
+```
---
+++ src/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <h1>Hello, world!</h1>
+  Counter: {{ counter.count }}
+  <button @click="counter.increment">Increment</button>
+</template>
