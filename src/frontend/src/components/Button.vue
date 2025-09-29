ac4487994f07d804913945bc7149a76d9238d2f5
Fernando Borretti <fernando@borretti.me>
2025-09-29 17:52:44 +1000

    components

---
+++ src/frontend/src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+}>()
+</script>
+
+<template>
+  <button>{{ label }}</button>
+</template>
+
+<style scoped>
+button {
+  border: 1px solid #bbb;
+  padding: 16px;
+  background: white;
+  font-size: 24px;
+  margin: 0 16px;
+  font-family: 'Times New Roman', 'Times', serif;
+  cursor: pointer;
+}
+</style>
---
+++ src/frontend/src/components/Spacer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="spacer"></div>
+</template>
+
+<style scoped>
+button {
+  border: 1px solid #bbb;
+  padding: 16px;
+  background: white;
+  font-size: 24px;
+  margin: 0 16px;
+  font-family: 'Times New Roman', 'Times', serif;
+  cursor: pointer;
+}
+
+.spacer {
+  flex: 1;
+}
+</style>
