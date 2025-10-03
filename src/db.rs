096de2f7218c0d450533e83bbff7ed9c6ba8df4b
Fernando Borretti <fernando@borretti.me>
2025-10-03 16:51:23 +1000

    trim

--- src/db.rs
+++ src/db.rs
@@ -30,39 +30,6 @@ use crate::types::performance::Performance;
 use crate::types::performance::ReviewedPerformance;
 use crate::types::timestamp::Timestamp;
 
-// Schema:
-// pragma foreign_keys = on;
-// create table cards (
-//     card_hash text primary key,
-//     added_at text not null,
-//     last_reviewed_at text,
-//     stability real,
-//     difficulty real,
-//     due_date text,
-//     review_count integer not null
-// ) strict;
-// create table sessions (
-//     session_id integer primary key,
-//     started_at text not null,
-//     ended_at text not null
-// ) strict;
-// create table reviews (
-//     review_id integer primary key,
-//     session_id integer not null
-//         references sessions (session_id)
-//         on update cascade
-//         on delete cascade,
-//     card_hash text not null
-//         references cards (card_hash)
-//         on update cascade
-//         on delete cascade,
-//     reviewed_at text not null,
-//     grade text not null,
-//     stability real not null,
-//     difficulty real not null,
-//     due_date text not null
-// ) strict;
-
 pub struct Database {
     conn: Connection,
 }
