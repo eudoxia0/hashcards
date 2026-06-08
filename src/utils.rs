576fce1a5add9360faa6fda5f0e530955d8a25a1
Fernando Borretti <fernando@borretti.me>
2026-06-08 14:44:00 +1000

    docstring

--- src/utils.rs
+++ src/utils.rs
@@ -19,7 +19,7 @@ use tokio::time::sleep;
 
 use crate::error::Fallible;
 
-// max-age is one week in seconds.
+/// max-age is one week in seconds.
 pub const CACHE_CONTROL_IMMUTABLE: &str = "public, max-age=604800, immutable";
 
 pub async fn wait_for_server(host: &str, port: u16) -> Fallible<()> {
