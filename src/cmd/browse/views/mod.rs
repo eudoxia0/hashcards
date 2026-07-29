7b508dfa067ce84eda9e9853546cedf4611ba674
Fernando Borretti <fernando@borretti.me>
2026-07-29 15:34:19 +1000

    add views module

--- src/cmd/browse/views/mod.rs
+++ src/cmd/browse/views/mod.rs
@@ -11,9 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-mod server;
-mod views;
-
-pub use server::BrowseServerConfig;
-pub use server::start_browse_server;
